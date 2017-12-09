30d6354f890ac9a1da37170178628a354384c1b6
Xavier Burrow <jxburrow@gmail.com>
2017-12-09 13:24:26 +1100

    text updates, random art updates

--- nav.php
+++ nav.php
@@ -9,7 +9,7 @@
 
 <div class='menu'>
   <div class='menu__inner'>
-    <div class='menu-close'>&times;</div>
+    <div class='menu-close'>Back</div>
     <div class='menu__content'>
       <div class='menu__content__inner text-medium'>
         <div class='menu__content__canvas hidden'>
@@ -17,13 +17,13 @@
         </div>
         <div class='menu__content__text hidden'>
           <br />
-          M E C H A N I C S<span class='mobile-hide'>&nbsp;<span class='red'>&times;</span>&nbsp;F E S T I V A L</span><br /><br />
+          MECHANICS<span class='mobile-hide'>&nbsp;<span class='red'>&times;</span>&nbsp;FESTIVAL</span><br /><br />
           <div class='text-centre'>
-            BOSTON&nbsp;<span class='red'>FALL 2018</span><br />
-            LINE-UP DROPS&nbsp;<span class='red'>FEBRUARY</span><br />
-            TICKETS&nbsp;<span class='red'>APRIL</span><br />
+            Boston&nbsp;<span class='red'>FALL 2018</span>
+            Line-up&nbsp;<span class='red'>FEBRUARY</span>
+            Tickets&nbsp;<span class='red'>APRIL</span>
           </div>
-          <br />
+          <br /><br /><br />
         </div>
       </div>
     </div>
