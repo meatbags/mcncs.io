17f523e5418d4bb3d7178aae1f89dc7dac197b80
Xavier Burrow <jxburrow@gmail.com>
2017-12-14 22:59:05 +1100

    logo progress, menu structuring etc

--- nav.php
+++ nav.php
@@ -2,35 +2,52 @@
 <div class='nav hidden'>
   <div class='nav__inner'>
     <div class='menu-button'>
-      <div class='menu-button__inner'>&#8707;</div>
+      <div class='menu-button__inner'>
+        <img src='<?php echo get_template_directory_uri(); ?>/lib/img/logo_white_tiny.png' />
+      </div>
+
+      <!--
+      <div class='menu-button__inner rotate'>&#8707;</div>
+      -->
     </div>
   </div>
 </div>
 
 <div class='menu'>
   <div class='menu__inner'>
-    <div class='menu-close'>BACK</div>
     <div class='menu__content'>
-      <div class='menu__content__inner text-medium'>
-        <div class='menu__content__canvas hidden'>
-          <!-- 2d canvas loads here -->
-        </div>
-        <div class='menu__content__text hidden'>
-          <br />
-          MECHANICS<span class='mobile-hide'>&nbsp;<span class='red'>&times;</span>&nbsp;FESTIVAL</span><br /><br />
-          <div class='text-centre'>
-            Boston&nbsp;<span class='red'>FALL 2018</span>
-            Line-up&nbsp;<span class='red'>FEBRUARY</span>
-            Tickets&nbsp;<span class='red'>APRIL</span>
+      <div class='menu__content__inner'>
+        <div class='row grid cascade'>
+          <div class='grid__third'>
+            <img src='<?php echo get_template_directory_uri(); ?>/lib/img/logo_white_2.png' alt='Mechanics Logo' />
           </div>
-          Mechanics is a 2-day contemporary electronic,
-          digital and experimental music and arts festival
-          that explores place making and the state of sound
-          practice in a confusing present.
-          <br /><br />
-          <a target='_blank' href=''>More info</a>
+        </div>
+        <div class='row cascade'>
+          <span class='highlight'>MECHANICS</span> is a two-day contemporary electronic, digital and
+          experimental music and arts festival exploring place-making
+          and the state of sound practice in a confusing present.
+        </div>
+        <div class='row cascade'>
+          <div class='grid'>
+            <div class='grid__30'>BOSTON</div>
+            <div class='grid__30 highlight'>FALL 2018</div>
+          </div>
+          <div class='grid'>
+            <div class='grid__30'>LINE UP COMING</div>
+            <div class='grid__30 highlight'>FEBRUARY</div>
+          </div>
+          <div class='grid'>
+            <div class='grid__30'>TICKETS</div>
+            <div class='grid__30 highlight'>APRIL</div>
+          </div>
+        </div>
+        <div class='row cascade'>
+          <span class='highlight'>CONNECT</span>
         </div>
       </div>
     </div>
+    <!--
+      <div class='menu-close'>BACK</div>
+    -->
   </div>
 </div>
